57b93719ceee2daba147d3db775db2a6cf30e41d
uodeeb <uodeeb@gmail.com>
2018-07-15 13:22:34 +0200

    Project Plan

--- js/app.js
+++ js/app.js
@@ -1,39 +1,48 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/* ____________________ Project Plan ____________________*/
+/* 
+A) HTML asets !--------------------------------------------!
+>>>>> The game is totally built upon calling javascript 
+>>>>> and HTML canvas element and 2d drawing
+B) Game logic !--------------------------------------------!
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+                    *1* game |starts| 
+                        on page load
+        --------------------< >------------------                    
+    <---|                                       |--->
+    |---> *2(a)*                       *2(b)* <<----|
+    Player char                             Enemies char
+    Initial position _________________cross the canvas(loop)
+------
+|--->>>*3* player move when direction keys pressed 
+(one step within a box each time and can not go out the canvas)
+        --------------------< >------------------                    
+    <---|                                       |--->
+    |---> *4(a)*                       *4(b)* <<----|
+player reach the other side_______player clashed with an enemy
+    |---> *5(a)*                       *5(b)* <<----|
+- add winner music alert    - add collision music alert
+        ___                                 ___
+         |                                   |
+         |>>>>>>>>>>>*6*game restart>>>>>>>>>|
+----------------------------------------------------
+____________________________________________________
+/* _________________ the real code _______________*/
+
+var Enemy = function() {
+
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+   
 };
 
-// Draw the enemy on the screen, required method for game
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
